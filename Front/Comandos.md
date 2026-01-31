c1bb928d3e00e4358aec0e1d009ad7f8ab2e8950
paolapulgar66 <paolapulgar66@gmail.com>
2026-01-31 15:07:34 -0500

    se avanzo en la creacion de clase y se agregaron nuevos servicios

---
+++ Front/Comandos.md
@@ -0,0 +1,10 @@
+| comando                                                       | funcion                            |
+| ------------------------------------------------------------- | ---------------------------------- |
+| mode -v                                                       | version de node                    |
+| npm -v                                                        | gestor de paquetes node.js         |
+| nvm install 22                                                | instalar version requerida         |
+| nvm list                                                      | verificar listas versiones de node |
+| set-ExecutionPolicy RemoteSigned -Scope CurrentUser           | habilitar permisos                 |
+| nvm use xx                                                    | Para usar versión especifica       |
+| nnm install -g @angular/cli                                   | instalar @angular/cli              |
+| ng new mi-primer-angular --standalone -- routing -- style=css | creacion de proyecto en angular    |
--- Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
@@ -1,18 +1,52 @@
 package com.empresa.gestion.model;
-public class Persona{
-    // Encapsulacion 
-    private String nombre;
-    private int edad;
-    // Contructor
-    public Persona(String nombre, int edad){
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
 
-    }
+public class Persona {
+  protected int id;
+  private String nombre;
+  protected int edad;
+  protected boolean active;
+  protected char genero;
+
+  public Persona( int id, String nombre, int edad, boolean active, char genero) {
+    this.id = id;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.active = active;
+    this.genero = genero;
+  }
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+  public char getGenero() {
+    return genero;
+  }
+  public void setGenero(char genero) {
+    this.genero = genero;
+  }
+
+  //método de la instancia
+  public void presentarse() {
+    System.out.println("hola, soy " + nombre + " y tengo " + edad + " años.");
+  }
+  public void mostrarInfoBasica() {
+    System.out.println("Nombre: "+nombre);
+    System.out.println("Edad: "+edad);
+    System.out.println("Genero: "+genero);
+    System.out.println("Estado: "+active);
+  }
+
+  public int getId() {
+    return id;
+  }
 }
